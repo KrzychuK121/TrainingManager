dcbaebef7fc57fd7c6baf983e2ce13867c6681c1
Komputer Domowy <KrzychuK1211@gmail.com>
2024-02-15 15:41:50 +0100

    Changing old methods responsible for creation of the objects. Now it is using two more methods which are responsible for: * looking for existing objects that are the same as sended by user and then replaceing them (there will be no duplicated objects) * setting created object to the relation object in the list (so there will be row in relation table)

--- src/main/java/springweb/trainingmanager/services/ExerciseService.java
+++ src/main/java/springweb/trainingmanager/services/ExerciseService.java
@@ -2,22 +2,86 @@ package springweb.trainingmanager.services;
 
 import org.springframework.stereotype.Service;
 import springweb.trainingmanager.models.entities.Exercise;
+import springweb.trainingmanager.models.entities.Training;
+import springweb.trainingmanager.models.viewmodels.exercise.ExerciseWrite;
+import springweb.trainingmanager.models.viewmodels.training.TrainingExercise;
 import springweb.trainingmanager.repositories.forcontrollers.ExerciseRepository;
+import springweb.trainingmanager.repositories.forcontrollers.TrainingRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class ExerciseService {
     private final ExerciseRepository repository;
+    private final TrainingRepository trainingRepository;
 
     public ExerciseService(
-        ExerciseRepository repository
+        final ExerciseRepository repository,
+        final TrainingRepository trainingRepository
     ) {
         this.repository = repository;
+        this.trainingRepository = trainingRepository;
     }
 
-    public Exercise create(Exercise toSave){
-        return repository.save(toSave);
+    /**
+     * This method is used to find existing trainings in database or creating new one
+     * corresponding to objects inside <code>trainings</code>. After that, they are
+     * returned as a new list.
+     *
+     * @param trainings list of <code>TrainingExercise</code> from <code>ExerciseWrite</code> object.
+ *                      Can be used e.g. in <code>create(ExerciseWrite toSave)</code> method.
+     *
+     * @return prepared list with <code>TrainingExercise</code> (founded in database or just created)
+     */
+    private List<TrainingExercise> prepTrainings(List<TrainingExercise> trainings){
+        if(trainings == null || trainings.isEmpty())
+            return null;
+
+        List<TrainingExercise> trainingToSave = new ArrayList<>(trainings.size());
+        trainings.forEach(
+            trainingExercise -> {
+                Training found = trainingRepository.findByTraining(trainingExercise.toTraining())
+                .orElse(trainingExercise.toTraining());
+
+                if(found.getId() == 0){
+                    var savedTraining = trainingRepository.save(found);
+                    trainingToSave.add(new TrainingExercise(savedTraining, savedTraining.getId()));
+                }else
+                    trainingToSave.add(new TrainingExercise(found, found.getId()));
+
+            }
+        );
+        return trainingToSave;
+    }
+
+    public Exercise create(ExerciseWrite toSave){
+        List<TrainingExercise> preparedTrainingList = prepTrainings(toSave.getTrainings());
+        if(preparedTrainingList != null)
+            toSave.setTrainings(preparedTrainingList);
+
+        var created = repository.save(toSave.toExercise());
+        addExerciseToTrainings(created, created.getTrainings());
+
+        return created;
+    }
+
+    /**
+     * This method <b>SHOULD</b> be used after <code>create(ExerciseWrite toSave)</code>.
+     * It is responsible for adding <code>toAdd</code> to every <code>toEdit</code> element
+     * and then saving the changes in the database. This operation is required to create proper
+     * many to many row between <code>Exercise</code> and <code>Training</code>
+     *
+     * @param toAdd <code>Exercise</code> with id which should be connected with <code>Training</code>
+     * @param toEdit list of <code>Training</code> objects which should be connected with <code>Exercise</code>
+     */
+    private void addExerciseToTrainings(Exercise toAdd, List<Training> toEdit){
+        toEdit.forEach(
+            training -> {
+                training.getExercises().add(toAdd);
+                trainingRepository.save(training);
+            }
+        );
     }
 
     public List<Exercise> getAll(){
--- src/main/java/springweb/trainingmanager/services/TrainingService.java
+++ src/main/java/springweb/trainingmanager/services/TrainingService.java
@@ -1,36 +1,96 @@
 package springweb.trainingmanager.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import springweb.trainingmanager.models.entities.Exercise;
 import springweb.trainingmanager.models.entities.Training;
+import springweb.trainingmanager.models.viewmodels.exercise.ExerciseTraining;
+import springweb.trainingmanager.models.viewmodels.exercise.ExerciseWrite;
+import springweb.trainingmanager.models.viewmodels.training.TrainingExercise;
+import springweb.trainingmanager.models.viewmodels.training.TrainingWrite;
 import springweb.trainingmanager.repositories.forcontrollers.ExerciseRepository;
 import springweb.trainingmanager.repositories.forcontrollers.TrainingRepository;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class TrainingService {
-    private final ExerciseService exerciseService;
     private final ExerciseRepository exerciseRepository;
     private final TrainingRepository repository;
+    private static final Logger logger = LoggerFactory.getLogger(TrainingService.class);
 
     public TrainingService(
-            final ExerciseService exerciseService,
             final ExerciseRepository exerciseRepository,
             final TrainingRepository repository
     ) {
-        this.exerciseService = exerciseService;
         this.exerciseRepository = exerciseRepository;
         this.repository = repository;
     }
 
-    public Training save(Training toSave){
-        toSave
-            .getExercises()
-            .forEach(
-                exercise -> exercise.setTraining(toSave)
-            );
-        return repository.save(toSave);
+    /**
+     * This method is used to find existing exercises in database or creating new one
+     * corresponding to objects inside <code>exercises</code>. After that, they are
+     * returned as a new list.
+     *
+     * @param exercises list of <code>ExerciseTraining</code> from <code>TrainingWrite</code> object.
+     *                      Can be used e.g. in <code>create(TrainingWrite toSave)</code> method.
+     *
+     * @return prepared list with <code>ExerciseTraining</code> (founded in database or just created)
+     */
+    private List<ExerciseTraining> prepExercises(List<ExerciseTraining> exercises) {
+        if(exercises == null || exercises.isEmpty())
+            return null;
+
+        List<ExerciseTraining> exerciseToSave = new ArrayList<>(exercises.size());
+        exercises.forEach(
+            exerciseTraining -> {
+                Exercise found = exerciseRepository.findByExercise(exerciseTraining.toExercise())
+                    .orElse(exerciseTraining.toExercise());
+
+                if(found.getId() == 0){
+                    logger.info("Nie znaleziono");
+                    var savedExercise = exerciseRepository.save(found);
+                    logger.info("Nowy id: " + savedExercise.getId());
+                    exerciseToSave.add(new ExerciseTraining(savedExercise));
+                }else{
+                    logger.info("Znaleziono");
+                    exerciseToSave.add(new ExerciseTraining(found));
+                }
+
+            }
+        );
+        return exerciseToSave;
+    }
+
+    public Training create(TrainingWrite toSave){
+        List<ExerciseTraining> preparedExerciseList = prepExercises(toSave.getExercises());
+        if(preparedExerciseList != null)
+            toSave.setExercises(preparedExerciseList);
+
+        var created = repository.save(toSave.toTraining());
+        addTrainingToExercises(created, created.getExercises());
+
+        return created;
+    }
+
+    /**
+     * This method <b>SHOULD</b> be used after <code>create(TrainingWrite toSave)</code>.
+     * It is responsible for adding <code>toAdd</code> to every <code>toEdit</code> element
+     * and then saving the changes in the database. This operation is required to create proper
+     * many to many row between <code>Exercise</code> and <code>Training</code>
+     *
+     * @param toAdd <code>Training</code> with id which should be connected with <code>Exercise</code>
+     * @param toEdit list of <code>Exercise</code> objects which should be connected with <code>Training</code>
+     */
+    private void addTrainingToExercises(Training toAdd, List<Exercise> toEdit){
+        toEdit.forEach(
+            exercise -> {
+                exercise.getTrainings().add(toAdd);
+                exerciseRepository.save(exercise);
+            }
+        );
     }
 
     public List<Training> getAll(){
