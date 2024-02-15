c072c2a3a7edd6b3bbde47d365bfcbb600993a0a
Komputer Domowy <KrzychuK1211@gmail.com>
2024-02-15 15:36:53 +0100

    Creating two new view models responsible for representing the objects without lists of its relationships (used in other view models to solve the infinite loop problem with many to many relationship)

---
+++ src/main/java/springweb/trainingmanager/models/viewmodels/exercise/ExerciseTraining.java
@@ -0,0 +1,106 @@
+package springweb.trainingmanager.models.viewmodels.exercise;
+
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.Range;
+import org.springframework.format.annotation.DateTimeFormat;
+import springweb.trainingmanager.models.entities.Exercise;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExerciseTraining {
+
+    private int id;
+    @NotBlank(message = "Nazwa ćwiczenia jest wymagana")
+    @Length(min = 3, max = 100, message = "Nazwa ćwiczenia musi mieścić się między 3 a 100 znaków")
+    private String name;
+    @NotBlank(message = "Opis nie może być pusty")
+    @Length(min = 3, max = 300, message = "Opis musi mieścić się między 3 a 300 znaków")
+    private String description;
+    // ile serii
+    @Range(min = 1, max = 10, message = "Ilość serii musi mieścić się między 1 a 10")
+    private int rounds;
+    // powtórzenia
+    @Range(min = 0, max = 100, message = "Ilość powtórzeń musi mieścić się między 0 a 100")
+    private int repetition;
+    @DateTimeFormat(pattern = "HH:mm:ss")
+    private LocalTime time;
+
+    public ExerciseTraining(){ }
+
+    public ExerciseTraining(Exercise exercise) {
+        this.id = exercise.getId();
+        this.name = exercise.getName();
+        this.description = exercise.getDescription();
+        this.rounds = exercise.getRounds();
+        this.repetition = exercise.getRepetition();
+        this.time = exercise.getTime();
+    }
+
+    public static List<ExerciseTraining> toExerciseTrainingList(final List<Exercise> list){
+        List<ExerciseTraining> result = new ArrayList<>(list.size());
+        list.forEach(exercise -> result.add(new ExerciseTraining(exercise)));
+        return result;
+    }
+
+    public static List<Exercise> toExerciseList(final List<ExerciseTraining> list){
+        List<Exercise> result = new ArrayList<>(list.size());
+        list.forEach(exerciseTraining -> {
+            var toSave = new Exercise();
+
+            toSave.setId(exerciseTraining.id);
+            toSave.setName(exerciseTraining.name);
+            toSave.setDescription(exerciseTraining.description);
+            toSave.setRounds(exerciseTraining.rounds);
+            toSave.setRepetition(exerciseTraining.repetition);
+            toSave.setTime(exerciseTraining.time);
+
+            result.add(toSave);
+        });
+
+        return result;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getRounds() {
+        return rounds;
+    }
+
+    public int getRepetition() {
+        return repetition;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public Exercise toExercise(){
+        var toReturn = new Exercise();
+
+        toReturn.setName(name);
+        toReturn.setDescription(description);
+        toReturn.setRounds(rounds);
+        toReturn.setRepetition(repetition);
+        toReturn.setTime(time);
+
+        return toReturn;
+    }
+
+}
---
+++ src/main/java/springweb/trainingmanager/models/viewmodels/training/TrainingExercise.java
@@ -0,0 +1,79 @@
+package springweb.trainingmanager.models.viewmodels.training;
+
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.Length;
+import springweb.trainingmanager.models.entities.Training;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TrainingExercise {
+    private final int id;
+    @NotBlank(message = "Tytuł treningu jest wymagany")
+    @Length(min = 3, max = 100, message = "Tytuł musi mieścić się między 3 a 100 znaków")
+    protected String title;
+    @NotBlank(message = "Opis nie może być pusty")
+    @Length(min = 3, max = 300, message = "Opis musi mieścić się między 3 a 300 znaków")
+    protected String description;
+
+    public TrainingExercise(){
+        id = 0;
+    }
+
+    public TrainingExercise(Training training, int id) {
+        this.id = id;
+        this.title = training.getTitle();
+        this.description = training.getDescription();
+    }
+
+    public static List<TrainingExercise> toTrainingExerciseList(final List<Training> list){
+        List<TrainingExercise> result = new ArrayList<>(list.size());
+        list.forEach(training -> result.add(new TrainingExercise(training, training.getId())));
+        return result;
+    }
+
+    public static List<Training> toTrainingList(final List<TrainingExercise> list){
+        List<Training> result = new ArrayList<>(list.size());
+        list.forEach(trainingExercise -> {
+            var toSave = new Training();
+
+            toSave.setId(trainingExercise.getId());
+            toSave.setId(trainingExercise.getId());
+            toSave.setTitle(trainingExercise.title);
+            toSave.setDescription(trainingExercise.description);
+
+            result.add(toSave);
+        });
+
+        return result;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Training toTraining(){
+        var toReturn = new Training();
+
+        toReturn.setTitle(title);
+        toReturn.setDescription(description);
+
+        return toReturn;
+    }
+}
