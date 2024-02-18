2b32e5ea6fc283cc846ae130a5a674939244827e
Komputer Domowy <KrzychuK1211@gmail.com>
2024-02-18 16:11:39 +0100

    Creating bean responsible for security configurations

---
+++ src/main/java/springweb/trainingmanager/security/SecurityConfig.java
@@ -0,0 +1,64 @@
+package springweb.trainingmanager.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(
+    jsr250Enabled = true,
+    securedEnabled = true,
+    prePostEnabled = true
+)
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests((authz) -> authz
+                .requestMatchers("/").permitAll()
+                .requestMatchers("/glowna").permitAll()
+            )
+            .formLogin(
+                formLogin -> formLogin.loginPage("/login").permitAll()
+                    .defaultSuccessUrl("/glowna")
+                    .successHandler( new SavedRequestAwareAuthenticationSuccessHandler())
+            )
+            .logout(withDefaults())
+            .exceptionHandling( exep -> exep
+                .accessDeniedPage("/access-denied")
+            );
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    /*@Bean
+    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails user = User.withUsername("user")
+                .password(passwordEncoder.encode("user"))
+                .roles("USER")
+                .build();
+        UserDetails admin = User.withUsername("admin")
+                .password(passwordEncoder.encode("admin"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }*/
+}
