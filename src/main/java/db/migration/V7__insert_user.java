64b949182b0e284a8ef8a112c3d8d9aad90e0ec8
KrzychuK121 <krzychuk1211@gmail.com>
2024-02-29 13:49:36 +0100

    importing new security packages

--- src/main/java/db/migration/V7__insert_user.java
+++ src/main/java/db/migration/V7__insert_user.java
@@ -2,7 +2,6 @@ package db.migration;
 
 import org.flywaydb.core.api.migration.BaseJavaMigration;
 import org.flywaydb.core.api.migration.Context;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import springweb.trainingmanager.models.entities.User;
