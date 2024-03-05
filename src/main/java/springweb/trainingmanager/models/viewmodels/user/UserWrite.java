8896c38da247c3ba64e83b0402da06ff8c57d5fb
Komputer Domowy <KrzychuK1211@gmail.com>
2024-03-05 13:12:00 +0100

    Creating implementation of the UserWrite DTO. Making it extends UserSchema

--- src/main/java/springweb/trainingmanager/models/viewmodels/user/UserWrite.java
+++ src/main/java/springweb/trainingmanager/models/viewmodels/user/UserWrite.java
@@ -1,4 +1,38 @@
 package springweb.trainingmanager.models.viewmodels.user;
 
-public class UserWrite {
+import jakarta.persistence.Transient;
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import springweb.trainingmanager.models.entities.User;
+import springweb.trainingmanager.models.schemas.UserSchema;
+
+public class UserWrite extends UserSchema {
+    @Transient
+    @NotBlank(message = "Powtórz hasło.")
+    private String passwordRepeat;
+    @Transient
+    private final PasswordEncoder encoder;
+
+    public UserWrite(final PasswordEncoder encoder) {
+        this.encoder = encoder;
+    }
+
+    public String getPasswordRepeat() {
+        return passwordRepeat;
+    }
+
+    public void setPasswordRepeat(String passwordRepeat) {
+        this.passwordRepeat = passwordRepeat;
+    }
+
+    public User toUser(){
+        User toReturn = new User();
+
+        toReturn.setUsername(username);
+        toReturn.setFirstName(firstName);
+        toReturn.setLastName(lastName);
+        toReturn.setPasswordHashed(encoder.encode(password));
+
+        return toReturn;
+    }
 }
