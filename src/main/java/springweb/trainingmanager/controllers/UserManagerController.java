eaf0d966d246acefebeeeb9aa4c010b0bc464022
Komputer Domowy <KrzychuK1211@gmail.com>
2024-03-05 13:09:52 +0100

    Changed constructor parameters which will be used in controllers actions. Removing seed method because seeding is being provided now by flyway migrations. Creating new register actions

--- src/main/java/springweb/trainingmanager/controllers/UserManagerController.java
+++ src/main/java/springweb/trainingmanager/controllers/UserManagerController.java
@@ -3,34 +3,38 @@ package springweb.trainingmanager.controllers;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import springweb.trainingmanager.models.entities.Role;
 import springweb.trainingmanager.models.entities.User;
+import springweb.trainingmanager.models.viewmodels.user.UserWrite;
 import springweb.trainingmanager.repositories.forcontrollers.RoleRepository;
 import springweb.trainingmanager.repositories.forcontrollers.UserRepository;
+import springweb.trainingmanager.services.UserService;
 
 import java.util.Set;
 
 @Controller
 public class UserManagerController {
-    private final UserRepository userRepo;
-    private final RoleRepository roleRepo;
+    private final UserService service;
     private final PasswordEncoder encoder;
     private static final Logger logger = LoggerFactory.getLogger(UserManagerController.class);
 
 
     public UserManagerController(
-        final UserRepository userRepo,
-        final RoleRepository roleRepo,
+        final UserService service,
         final PasswordEncoder encoder
     ) {
-        this.userRepo = userRepo;
-        this.roleRepo = roleRepo;
+        this.service = service;
         this.encoder = encoder;
     }
 
@@ -40,6 +44,37 @@ public class UserManagerController {
     }
     @GetMapping("/login")
     String login(){
+        return "userManager/login";
+    }
+
+    @GetMapping("/register")
+    String register(Model model){
+        model.addAttribute("user", new UserWrite(encoder));
+        return "userManager/register";
+    }
+
+    @PostMapping("/register")
+    String register(
+        @ModelAttribute("user") @Valid UserWrite current,
+        BindingResult result,
+        Model model
+    ){
+        if(!service.ifPasswordsMatches(current.getPassword(), current.getPasswordRepeat()))
+            result.addError(new ObjectError("passwordRepeat", UserService.PASSWORDS_NOT_EQUAL_MESSAGE));
+        if(result.hasErrors())
+            return "userManager/register";
+
+        try {
+            service.register(current);
+        } catch (IllegalArgumentException e){
+            logger.error("Wystąpił wyjątek: " + e.getMessage());
+            model.addAttribute("messType", "danger");
+            model.addAttribute("mess", e.getMessage());
+            return "userManager/register";
+        }
+
+        model.addAttribute("messType", "success");
+        model.addAttribute("mess", "Rejestracja przeszła pomyślnie!\nSpróbuj się zalogować");
         return "userManager/login";
     }
 
@@ -60,23 +95,5 @@ public class UserManagerController {
         request.logout();
         return "userManager/logout";
     }
-
-    /*@GetMapping("/seed")
-    String seed(){
-        String pass = "admin1234";
-        User admin = new User();
-        admin.setUsername("admin");
-        admin.setFirstName("Admin");
-        admin.setLastName("Admin");
-        admin.setPassword(pass);
-        admin.setPasswordHashed(encoder.encode(pass));
-        Role role = new Role();
-        role.setName("ROLE_ADMIN");
-        admin.setRoles(Set.of(role));
-        role.setUsers(Set.of(admin));
-        roleRepo.save(role);
-        userRepo.save(admin);
-        return "index2";
-    }*/
 
 }
