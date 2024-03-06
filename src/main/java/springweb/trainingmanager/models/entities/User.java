27f31e6a9147a2e5a5a7a6392ecd43ed88b699f0
Komputer Domowy <KrzychuK1211@gmail.com>
2024-03-06 14:33:45 +0100

    Creating relation many to many with: exercises, trainings

--- src/main/java/springweb/trainingmanager/models/entities/User.java
+++ src/main/java/springweb/trainingmanager/models/entities/User.java
@@ -3,6 +3,7 @@ package springweb.trainingmanager.models.entities;
 import jakarta.persistence.*;
 import springweb.trainingmanager.models.schemas.UserSchema;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -17,6 +18,22 @@ public class User extends UserSchema {
         inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
     )
     private Set<Role> roles;
+
+    @ManyToMany
+    @JoinTable(
+        name = "users_trainings",
+        joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+        inverseJoinColumns = @JoinColumn(name = "training_id", referencedColumnName = "id")
+    )
+    private Set<Training> trainings = new HashSet<>();
+
+    @ManyToMany
+    @JoinTable(
+        name = "users_exercises",
+        joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+        inverseJoinColumns = @JoinColumn(name = "exercise_id", referencedColumnName = "id")
+    )
+    private Set<Exercise> exercises = new HashSet<>();
 
     public User() { }
 
@@ -35,4 +52,22 @@ public class User extends UserSchema {
     public void setRoles(Set<Role> roles) {
         this.roles = roles;
     }
+
+    public Set<Training> getTrainings() {
+        return trainings;
+    }
+
+    public void setTrainings(Set<Training> trainings) {
+        this.trainings = trainings;
+    }
+
+    public Set<Exercise> getExercises() {
+        return exercises;
+    }
+
+    public void setExercises(Set<Exercise> exercises) {
+        this.exercises = exercises;
+    }
 }
+
+
