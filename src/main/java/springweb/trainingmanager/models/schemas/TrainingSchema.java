4e1865374239092762aec8950cd96396cc8ee2cb
Komputer Domowy <KrzychuK1211@gmail.com>
2024-03-06 11:31:40 +0100

    Creating schema for Training so it contains all fields and getters uses in every other entities and views

---
+++ src/main/java/springweb/trainingmanager/models/schemas/TrainingSchema.java
@@ -0,0 +1,46 @@
+package springweb.trainingmanager.models.schemas;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.Length;
+import springweb.trainingmanager.models.entities.Training;
+
+import java.util.Objects;
+
+@MappedSuperclass
+public abstract class TrainingSchema {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected int id;
+    @NotBlank(message = "Tytuł treningu jest wymagany")
+    @Length(min = 3, max = 100, message = "Tytuł musi mieścić się między 3 a 100 znaków")
+    protected String title;
+    @NotBlank(message = "Opis nie może być pusty")
+    @Length(min = 3, max = 300, message = "Opis musi mieścić się między 3 a 300 znaków")
+    protected String description;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TrainingSchema training = (TrainingSchema) o;
+        return id == training.id &&
+                Objects.equals(title, training.title) &&
+                Objects.equals(description, training.description);
+    }
+}
