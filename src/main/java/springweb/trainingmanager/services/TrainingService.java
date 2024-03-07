1b6a4d76d2220b4acc15f66da9d1c02a60f86a34
Komputer Domowy <KrzychuK1211@gmail.com>
2024-03-07 15:49:22 +0100

    Editing create method to connect user to new training if someone with ROLE_USER is logged in. Creating new method to update user info in training. Creating new method responsible for displaying list of trainings connected to user. Repairing delete method so now trainings connected to users can be deleted

--- src/main/java/springweb/trainingmanager/services/TrainingService.java
+++ src/main/java/springweb/trainingmanager/services/TrainingService.java
@@ -2,28 +2,37 @@ package springweb.trainingmanager.services;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import springweb.trainingmanager.models.entities.Exercise;
 import springweb.trainingmanager.models.entities.Training;
+import springweb.trainingmanager.models.entities.User;
 import springweb.trainingmanager.models.viewmodels.exercise.ExerciseTraining;
+import springweb.trainingmanager.models.viewmodels.training.TrainingRead;
 import springweb.trainingmanager.models.viewmodels.training.TrainingWrite;
 import springweb.trainingmanager.repositories.forcontrollers.ExerciseRepository;
 import springweb.trainingmanager.repositories.forcontrollers.TrainingRepository;
+import springweb.trainingmanager.repositories.forcontrollers.UserRepository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class TrainingService {
     private final ExerciseRepository exerciseRepository;
     private final TrainingRepository repository;
+    private final UserRepository userRepository;
 
     public TrainingService(
-            final ExerciseRepository exerciseRepository,
-            final TrainingRepository repository
+        final ExerciseRepository exerciseRepository,
+        final TrainingRepository repository,
+        final UserRepository userRepository
     ) {
         this.exerciseRepository = exerciseRepository;
         this.repository = repository;
+        this.userRepository = userRepository;
     }
 
     /**
@@ -57,12 +66,24 @@ public class TrainingService {
         return exerciseToSave;
     }
 
-    public Training create(TrainingWrite toSave){
+    public Training create(TrainingWrite toSave, String userId){
         List<Exercise> preparedExerciseList = prepExercises(toSave.getExercises());
         if(preparedExerciseList != null)
             toSave.setExercises(ExerciseTraining.toExerciseTrainingList(preparedExerciseList));
 
         var created = repository.save(toSave.toTraining());
+
+        if(userId != null){
+            User loggedUser = userRepository.findById(userId)
+                .orElseThrow(
+                    () -> new IllegalArgumentException("Nie istnieje użytkownik o takim numerze ID.")
+                );
+            Set<Training> oldUsersTrainings = loggedUser.getTrainings();
+            oldUsersTrainings.add(created);
+            loggedUser.setTrainings(oldUsersTrainings);
+            userRepository.save(loggedUser);
+        }
+
         if(preparedExerciseList != null)
             editTrainingInExercises(created, preparedExerciseList, true);
 
@@ -94,6 +115,20 @@ public class TrainingService {
         );
     }
 
+    private void editTrainingInUsers(Training toAddOrRemove, Set<User> toEdit, boolean ifAdd){
+        if(toEdit == null)
+            return;
+        toEdit.forEach(
+                user -> {
+                    if(ifAdd)
+                        user.getTrainings().add(toAddOrRemove);
+                    else
+                        user.getTrainings().remove(toAddOrRemove);
+                    userRepository.save(user);
+                }
+        );
+    }
+
     public List<Training> getAll(){
         return repository.findAll();
     }
@@ -103,6 +138,20 @@ public class TrainingService {
             .orElseThrow(
                 () -> new IllegalArgumentException("Nie znaleziono treningu o podanym numerze id")
             );
+    }
+
+    // TODO: Use it in controller when ROLE_USER registered, otherwise normal getAll
+    public List<TrainingRead> getByUserId(String userId){
+        List<TrainingRead> usersTrainings = userRepository.findById(userId)
+            .orElseThrow(
+                () -> new IllegalArgumentException("Użytkownik o takim numerze id nie istnieje.")
+            ).getTrainings().stream().map(
+                training -> {
+                    TrainingRead read = new TrainingRead(training);
+                    return read;
+                }
+            ).collect(Collectors.toList());
+        return usersTrainings;
     }
 
     public void edit(TrainingWrite toEdit, int id){
@@ -116,10 +165,11 @@ public class TrainingService {
         var saved = repository.save(toSave);
         editTrainingInExercises(saved, saved.getExercises(), true);
     }
-
     public void delete(int id){
         var toDelete = getById(id);
         editTrainingInExercises(toDelete, toDelete.getExercises(), false);
+        editTrainingInUsers(toDelete, toDelete.getUsers(), false);
+
         repository.deleteById(id);
     }
 
