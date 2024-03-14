4432e429af6ead4cd2881b7e92ba780f9e4a9687
KrzychuK121 <krzychuk1211@gmail.com>
2024-03-14 10:43:45 +0100

    First attempt to create sorting in exercises

--- src/main/java/springweb/trainingmanager/controllers/TrainingController.java
+++ src/main/java/springweb/trainingmanager/controllers/TrainingController.java
@@ -4,6 +4,7 @@ import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
