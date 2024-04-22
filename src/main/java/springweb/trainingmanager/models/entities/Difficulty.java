a065e47310b5fbc37cbc46112845e547b93fed16
Komputer Domowy <KrzychuK1211@gmail.com>
2024-04-22 12:29:18 +0200

    Creating enum that represents exercise difficulty

--- src/main/java/springweb/trainingmanager/models/entities/Difficulty.java
+++ src/main/java/springweb/trainingmanager/models/entities/Difficulty.java
@@ -6,12 +6,15 @@ public enum Difficulty {
     ADVANCED;
 
     public static String getEnumDesc(Difficulty difficulty){
+        if(difficulty == null)
+            return "brak";
         String toReturn = "";
 
         switch(difficulty){
             case FOR_BEGINNERS -> toReturn = "dla początkujących";
             case MEDIUM -> toReturn = "dla średniozaawansowanych";
             case ADVANCED -> toReturn = "dla zaawansowanych";
+            default -> toReturn ="nie zdefiniowano";
         }
 
         return toReturn;
