8dc5e9acd5d7290937122f7986b763eb19943cb7
Komputer Domowy <KrzychuK1211@gmail.com>
2024-04-22 15:45:05 +0200

    Changing method responsible for displaying names of difficulty and created field responsible for display info of difficulty. Now the code is cleaner

--- src/main/java/springweb/trainingmanager/models/entities/Difficulty.java
+++ src/main/java/springweb/trainingmanager/models/entities/Difficulty.java
@@ -1,22 +1,19 @@
 package springweb.trainingmanager.models.entities;
 
 public enum Difficulty {
-    FOR_BEGINNERS,
-    MEDIUM,
-    ADVANCED;
+    FOR_BEGINNERS("dla początkujących"),
+    MEDIUM("dla średniozaawansowanych"),
+    ADVANCED("dla zaawansowanych");
+
+    private final String display;
+
+    private Difficulty(String display){
+        this.display = display;
+    }
 
     public static String getEnumDesc(Difficulty difficulty){
         if(difficulty == null)
             return "brak";
-        String toReturn = "";
-
-        switch(difficulty){
-            case FOR_BEGINNERS -> toReturn = "dla początkujących";
-            case MEDIUM -> toReturn = "dla średniozaawansowanych";
-            case ADVANCED -> toReturn = "dla zaawansowanych";
-            default -> toReturn ="nie zdefiniowano";
-        }
-
-        return toReturn;
+        return difficulty.display;
     }
 }
