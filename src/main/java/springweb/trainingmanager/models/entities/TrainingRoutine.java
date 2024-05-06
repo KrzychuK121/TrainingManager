39224ef6a7ac3c4c94928d9baf2039269499c12f
Komputer Domowy <KrzychuK1211@gmail.com>
2024-05-06 13:59:46 +0200

    Adding entity that allows users to create new routines and plan when they want to train and what kind of training. They can also decide if they want to active or deactivate training routine (which routine they want to follow for now)

---
+++ src/main/java/springweb/trainingmanager/models/entities/TrainingRoutine.java
@@ -0,0 +1,34 @@
+package springweb.trainingmanager.models.entities;
+
+import jakarta.persistence.*;
+import springweb.trainingmanager.models.schemas.TrainingRoutineSchema;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "training_routine")
+public class TrainingRoutine extends TrainingRoutineSchema {
+
+    @ManyToMany
+    @JoinTable(
+        name = "training_routine_schedule",
+        joinColumns = { @JoinColumn(name = "routine_id", referencedColumnName = "id") },
+        inverseJoinColumns = {
+            @JoinColumn(name = "training_id", referencedColumnName = "training_id"),
+            @JoinColumn(name = "weekday", referencedColumnName = "weekday")
+        },
+        uniqueConstraints = {@UniqueConstraint(columnNames = { "routine_id", "weekday" })}
+    )
+    private Set<TrainingSchedule> schedules = new HashSet<>();
+    public TrainingRoutine() {
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
