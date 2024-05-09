4f44b4ac1608a037cf2689582b102300e40f8bc4
Komputer Domowy <KrzychuK1211@gmail.com>
2024-05-09 08:58:30 +0200

    Changing id type in repository (from composite key to one column integer key)

--- src/main/java/springweb/trainingmanager/repositories/beans/SqlTrainingScheduleRepository.java
+++ src/main/java/springweb/trainingmanager/repositories/beans/SqlTrainingScheduleRepository.java
@@ -3,12 +3,11 @@ package springweb.trainingmanager.repositories.beans;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import springweb.trainingmanager.models.entities.TrainingSchedule;
-import springweb.trainingmanager.models.entities.TrainingScheduleId;
 import springweb.trainingmanager.repositories.forcontrollers.TrainingScheduleRepository;
 
 @Repository
 interface SqlTrainingScheduleRepository
     extends TrainingScheduleRepository,
-            JpaRepository<TrainingSchedule, TrainingScheduleId> {
+            JpaRepository<TrainingSchedule, Integer> {
 
 }
