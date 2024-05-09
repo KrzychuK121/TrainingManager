a2282ccaaa68e24b9559bdfb35809129bca95158
Komputer Domowy <KrzychuK1211@gmail.com>
2024-05-09 08:47:55 +0200

    Creating TrainingPlan that previously was mapped as many to many relationship between TrainingRoutine and TrainingSchedule. It needed an entity because of trainingTime field (not only id's fields). Creating TrainingPlanId class to be the IdClass for TrainingPlan (two column primary key, not needed using another column as id)

---
+++ src/main/java/springweb/trainingmanager/models/entities/TrainingPlan.java
@@ -0,0 +1,52 @@
+package springweb.trainingmanager.models.entities;
+
+import jakarta.persistence.*;
+import springweb.trainingmanager.models.schemas.TrainingPlanId;
+import springweb.trainingmanager.models.schemas.TrainingPlanSchema;
+
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "training_plan")
+public class TrainingPlan extends TrainingPlanSchema {
+    @ManyToOne
+    @JoinColumn(
+        name = "schedule_id",
+        referencedColumnName = "id",
+        insertable = false,
+        updatable = false
+    )
+    private TrainingSchedule trainingSchedule;
+    @ManyToOne
+    @JoinColumn(
+        name = "routine_id",
+        referencedColumnName = "id",
+        insertable = false,
+        updatable = false
+    )
+    private TrainingRoutine trainingRoutine;
+
+    public TrainingPlan() {
+    }
+
+    public void setId(TrainingPlanId id){
+        trainingRoutineId = id.getTrainingRoutineId();
+        trainingScheduleId = id.getTrainingScheduleId();
+    }
+
+    public void setTrainingTime(LocalTime trainingTime) {
+        this.trainingTime = trainingTime;
+    }
+    public TrainingSchedule getTrainingSchedule() {
+        return trainingSchedule;
+    }
+    public void setTrainingSchedule(TrainingSchedule trainingSchedule) {
+        this.trainingSchedule = trainingSchedule;
+    }
+    public TrainingRoutine getTrainingRoutine() {
+        return trainingRoutine;
+    }
+    public void setTrainingRoutine(TrainingRoutine trainingRoutine) {
+        this.trainingRoutine = trainingRoutine;
+    }
+}
---
+++ src/main/java/springweb/trainingmanager/models/schemas/TrainingPlanSchema.java
@@ -0,0 +1,41 @@
+package springweb.trainingmanager.models.schemas;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalTime;
+
+@IdClass(TrainingPlanId.class)
+@MappedSuperclass
+public class TrainingPlanSchema {
+    @Id
+    @Column(name = "routine_id")
+    protected int trainingRoutineId;
+    @Id
+    @Column(name = "schedule_id")
+    protected int trainingScheduleId;
+    @DateTimeFormat(pattern = "hh:mm")
+    protected LocalTime trainingTime;
+
+    public TrainingPlanSchema() {
+    }
+
+
+    public int getTrainingRoutineId() {
+        return trainingRoutineId;
+    }
+    public int getTrainingScheduleId() {
+        return trainingScheduleId;
+    }
+    public TrainingPlanId getId(){
+        return new TrainingPlanId(trainingRoutineId, trainingScheduleId);
+    }
+    public LocalTime getTrainingTime() {
+        return trainingTime;
+    }
+
+
+}
