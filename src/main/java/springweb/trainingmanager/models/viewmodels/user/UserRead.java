ed303d5d3af1815655917bfb3c4bba0f25dbc355
Komputer Domowy <KrzychuK1211@gmail.com>
2024-05-14 19:31:36 +0200

    Implementing constructor for UserRead so it gets all values from User object

--- src/main/java/springweb/trainingmanager/models/viewmodels/user/UserRead.java
+++ src/main/java/springweb/trainingmanager/models/viewmodels/user/UserRead.java
@@ -1,6 +1,16 @@
 package springweb.trainingmanager.models.viewmodels.user;
 
+import springweb.trainingmanager.models.entities.User;
 import springweb.trainingmanager.models.schemas.UserSchema;
 
 public class UserRead extends UserSchema {
+
+    public UserRead(
+        User user
+    ) {
+        this.id = user.getId();
+        this.firstName = user.getFirstName();
+        this.lastName = user.getLastName();
+        this.username = user.getUsername();
+    }
 }
