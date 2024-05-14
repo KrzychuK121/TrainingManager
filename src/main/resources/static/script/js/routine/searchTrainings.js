810354ad407c93f7eb515990aee23cb30eb80a2a
Komputer Domowy <KrzychuK1211@gmail.com>
2024-05-14 19:47:35 +0200

    Creating js file that handles search bars and select lists in save.html (while trying to create new training plan). In that way theres no need to reload page while trying to filter trainings in one of the text fields.

---
+++ src/main/resources/static/script/js/routine/searchTrainings.js
@@ -0,0 +1,60 @@
+(async function (){
+    const trainingsResponse = await fetch("/training/api");
+    const weekdaysResponse = await fetch("/weekdays");
+    const allTrainings = await trainingsResponse.json();
+    const weekdays = await weekdaysResponse.json();
+
+    for(const weekday of weekdays){
+        const inputText = document.getElementById("search" + weekday);
+        const select = document.getElementById("training"+ weekday);
+
+        initSelect(allTrainings, select);
+
+        inputText.addEventListener(
+            'input', async (e) =>{
+                e.preventDefault();
+
+                while(select.firstChild)
+                    select.firstChild.remove();
+                createOption("--Wybierz trening--", -1, true, select);
+
+                const inputValue = e.currentTarget.value.toLowerCase();
+
+                if(inputValue.trim() === ""){
+                    initSelect(allTrainings, select);
+                    return;
+                }
+
+                console.log('wykonuje sie');
+
+                allTrainings.forEach(
+                    training => {
+                        const title = training.title.toLowerCase();
+
+                        if(title.includes(inputValue))
+                            createOption(`id: ${training.id} tytuł: ${training.title}`, training.id, false, select);
+                    }
+                );
+            }
+        );
+    }
+})();
+
+function createOption(text, value, selected, parent){
+    const newOption = document.createElement("option");
+    newOption.value = value;
+    newOption.selected = selected;
+    newOption.innerHTML = text;
+    parent.appendChild(newOption);
+}
+
+/**
+ * Creates default top few trainings in select list.
+ * Invoked while search bar is empty
+ */
+function initSelect(trainings, parent, howManyRows = 20){
+    for(let i = 0; i < howManyRows; i++){
+        const training = trainings[i];
+        createOption(`id: ${training.id} tytuł: ${training.title}`, training.id, false, parent);
+    }
+}
