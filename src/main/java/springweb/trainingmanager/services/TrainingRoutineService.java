e5c782aa74f8a9981412d54e7d6a70b4a3aea2d6
Komputer Domowy <KrzychuK1211@gmail.com>
2024-05-14 20:03:48 +0200

    Creating new service for routine that gets all routines and active routine for provided user

---
+++ src/main/java/springweb/trainingmanager/services/TrainingRoutineService.java
@@ -0,0 +1,32 @@
+package springweb.trainingmanager.services;
+
+import org.springframework.stereotype.Service;
+import springweb.trainingmanager.models.entities.TrainingRoutine;
+import springweb.trainingmanager.models.viewmodels.trainingRoutine.TrainingRoutineRead;
+import springweb.trainingmanager.repositories.forcontrollers.TrainingRoutineRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class TrainingRoutineService {
+
+    private final TrainingRoutineRepository repository;
+
+    public TrainingRoutineService(
+        final TrainingRoutineRepository repository
+    ) {
+        this.repository = repository;
+    }
+
+    public List<TrainingRoutineRead> getAll(){
+        return repository.findAll()
+        .stream().map(TrainingRoutineRead::new)
+        .collect(Collectors.toList());
+    }
+
+    public TrainingRoutine getUserActiveRoutine(String userId){
+        return repository.findByOwnerIdAndActiveTrue(userId)
+        .orElseThrow(() -> new IllegalStateException("Użytkownik nie posiada aktywnej rutyny treningowej."));
+    }
+}
