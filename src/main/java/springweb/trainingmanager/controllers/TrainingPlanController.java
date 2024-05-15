f5eda9fe2a536ffe0f91d130d3c63cb6d2c347bd
Komputer Domowy <KrzychuK1211@gmail.com>
2024-05-15 14:50:08 +0200

    Repairing getWeekCreate method to create write models containing all future needed data in view. Creating first implementation of weekCreate method and checking if it gains all necessary data from view and in method.

--- src/main/java/springweb/trainingmanager/controllers/TrainingPlanController.java
+++ src/main/java/springweb/trainingmanager/controllers/TrainingPlanController.java
@@ -6,14 +6,18 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.view.RedirectView;
 import springweb.trainingmanager.models.entities.TrainingPlan;
+import springweb.trainingmanager.models.entities.TrainingSchedule;
 import springweb.trainingmanager.models.entities.Weekdays;
+import springweb.trainingmanager.models.viewmodels.trainingPlan.TrainingPlansWrite;
 import springweb.trainingmanager.services.TrainingPlanService;
 import springweb.trainingmanager.services.UserService;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -21,6 +25,7 @@ import java.util.Map;
 @RequestMapping("/plans")
 public class TrainingPlanController {
     private final TrainingPlanService service;
+    private final Logger logger = LoggerFactory.getLogger(TrainingPlanController.class);
 
     public TrainingPlanController(
         final TrainingPlanService service
@@ -47,13 +52,44 @@ public class TrainingPlanController {
     }
 
     @GetMapping("/week/create")
-    public String getWeekCreate(Model model){
-        model.addAttribute("weekdays", Weekdays.values());
+    public String getWeekCreate(
+        Model model
+    ){
+        var weekdays = Weekdays.values();
+        TrainingPlansWrite toWrite = new TrainingPlansWrite();
+        for(var weekday : weekdays)
+            toWrite.add(weekday);
+
+        model.addAttribute("schedulesList", toWrite);
+        model.addAttribute("weekdays", weekdays);
         return "routine/save";
     }
 
     @PostMapping("/week/create")
-    public RedirectView weekCreate(){
+    public RedirectView weekCreate(
+        @ModelAttribute("schedulesList") TrainingPlansWrite schedulesList,
+        Authentication auth
+    ){
+        var weekdays = Weekdays.values();
+        List<TrainingPlan> plans = new ArrayList<>(weekdays.length);
+        for(var weekday : weekdays) {
+            var trainingId = schedulesList.getSchedules().get(weekday);
+            var toAdd = new TrainingPlan(
+                UserService.getUserByAuth(auth),
+                new TrainingSchedule(trainingId, weekday)
+            );
+            plans.add(toAdd);
+        }
+
+        for(var plan : plans){
+            var schedule = plan.getTrainingSchedule();
+            var routine = plan.getTrainingRoutine();
+            logger.info("plan: ");
+            logger.info("trainingId: " + schedule.getTrainingId());
+            logger.info("weekday: " + schedule.getWeekday());
+            logger.info("user: " + routine.getOwner().getFirstName());
+            logger.info("active: " + routine.isActive());
+        }
 
         return new RedirectView("/plans/week/create");
     }
