d522de313db43e9dd2bc140d4d26d8516b3cfa75
Komputer Domowy <KrzychuK1211@gmail.com>
2024-05-16 21:06:33 +0200

    Creating interface used in NoDuplicationService. Every write models should implement its methods

---
+++ src/main/java/springweb/trainingmanager/models/viewmodels/Castable.java
@@ -0,0 +1,10 @@
+package springweb.trainingmanager.models.viewmodels;
+
+/**
+ * This interface should be implemented by all write models
+ * that can be converted to entities that they are representing
+ * E - entity to return after convert
+ */
+public interface Castable<E> {
+    E toEntity();
+}
