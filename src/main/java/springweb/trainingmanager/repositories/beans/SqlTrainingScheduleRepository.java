8b01b4d7874baf5c87c04f1329d73091cb0a026f
Komputer Domowy <KrzychuK1211@gmail.com>
2024-05-16 21:11:52 +0200

    Making todos and declaration of method responsible for finding duplicated entities

--- src/main/java/springweb/trainingmanager/repositories/beans/SqlTrainingScheduleRepository.java
+++ src/main/java/springweb/trainingmanager/repositories/beans/SqlTrainingScheduleRepository.java
@@ -5,9 +5,13 @@ import org.springframework.stereotype.Repository;
 import springweb.trainingmanager.models.entities.TrainingSchedule;
 import springweb.trainingmanager.repositories.forcontrollers.TrainingScheduleRepository;
 
+import java.util.Optional;
+
 @Repository
 interface SqlTrainingScheduleRepository
     extends TrainingScheduleRepository,
             JpaRepository<TrainingSchedule, Integer> {
-
+    // TODO: Create "implementation" (using annotations) to find same entity as provided.
+    @Override
+    Optional<TrainingSchedule> findDuplication(TrainingSchedule entity);
 }
