1151a6d04d91dfcf278ddcbb86a0d80658355005
Komputer Domowy <KrzychuK1211@gmail.com>
2024-05-24 10:43:45 +0200

    Creating new method that finds list of training routines by User owner object

--- src/main/java/springweb/trainingmanager/repositories/forcontrollers/TrainingPlanRepository.java
+++ src/main/java/springweb/trainingmanager/repositories/forcontrollers/TrainingPlanRepository.java
@@ -1,6 +1,7 @@
 package springweb.trainingmanager.repositories.forcontrollers;
 
 import springweb.trainingmanager.models.entities.TrainingPlan;
+import springweb.trainingmanager.models.entities.User;
 import springweb.trainingmanager.models.schemas.TrainingPlanId;
 
 import java.util.List;
@@ -11,6 +12,7 @@ public interface TrainingPlanRepository {
     TrainingPlan save(TrainingPlan entity);
     Optional<TrainingPlan> findById(TrainingPlanId trainingPlanId);
     Optional<List<TrainingPlan>> findByTrainingRoutineId(int trainingRoutineId);
+    Optional<List<TrainingPlan>> findByTrainingRoutineOwner(User owner);
     boolean existsById(TrainingPlanId trainingPlanId);
     void deleteById(TrainingPlanId trainingPlanId);
 }
