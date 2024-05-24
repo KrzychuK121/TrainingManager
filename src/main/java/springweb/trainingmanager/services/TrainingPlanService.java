6b68a4200550f226cdc5cf0f656178badb7ddf31
Komputer Domowy <KrzychuK1211@gmail.com>
2024-05-24 10:46:07 +0200

    Creating new method that is used in controller and is responsible for creating training plans, new routine object and uses method to find or create new training routines objects. Creating methods that are used to list all training plans for routine/index view. Creating method that prepares training schedule

--- src/main/java/springweb/trainingmanager/services/TrainingPlanService.java
+++ src/main/java/springweb/trainingmanager/services/TrainingPlanService.java
@@ -1,12 +1,18 @@
 package springweb.trainingmanager.services;
 
 import org.springframework.stereotype.Service;
-import springweb.trainingmanager.models.entities.TrainingPlan;
-import springweb.trainingmanager.models.entities.Weekdays;
+import springweb.trainingmanager.models.entities.*;
+import springweb.trainingmanager.models.viewmodels.trainingPlan.TrainingPlanWrite;
+import springweb.trainingmanager.models.viewmodels.trainingPlan.TrainingPlansWrite;
+import springweb.trainingmanager.models.viewmodels.trainingRoutine.TrainingRoutineReadIndex;
+import springweb.trainingmanager.models.viewmodels.trainingSchedule.TrainingScheduleRead;
 import springweb.trainingmanager.repositories.forcontrollers.TrainingPlanRepository;
 import springweb.trainingmanager.repositories.forcontrollers.TrainingScheduleRepository;
 
+import java.time.DateTimeException;
+import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -16,15 +22,56 @@ public class TrainingPlanService {
     private final TrainingPlanRepository repository;
     private final TrainingScheduleRepository scheduleRepository;
     private final TrainingRoutineService routineService;
+    private final TrainingService trainingService;
 
     public TrainingPlanService(
         final TrainingPlanRepository repository,
         final TrainingScheduleRepository scheduleRepository,
-        final TrainingRoutineService routineService
+        final TrainingRoutineService routineService,
+        final TrainingService trainingService
     ) {
         this.repository = repository;
         this.scheduleRepository = scheduleRepository;
         this.routineService = routineService;
+        this.trainingService = trainingService;
+    }
+
+
+    private Map<Integer, TrainingRoutineReadIndex> getMapFromPlans(List<TrainingPlan> plans){
+        Map<Integer, TrainingRoutineReadIndex> toReturn = new HashMap<>();
+        plans.forEach(
+            trainingPlan -> {
+                var routine = trainingPlan.getTrainingRoutine();
+                var routineId = routine.getId();
+                if(!toReturn.containsKey(routineId))
+                    toReturn.put(
+                        routineId,
+                        new TrainingRoutineReadIndex(
+                            routine.isActive()
+                        )
+                    );
+
+                var weekday = trainingPlan.getTrainingSchedule().getWeekday();
+                toReturn.get(routineId).putSchedule(
+                    weekday,
+                    new TrainingScheduleRead(
+                        trainingPlan.getTrainingSchedule()
+                    )
+                );
+
+            }
+        );
+
+        return toReturn;
+    }
+
+    public List<TrainingRoutineReadIndex> getAllByUser(User owner){
+        List<TrainingPlan> all = repository.findByTrainingRoutineOwner(owner)
+            .orElseThrow(() -> new IllegalArgumentException("Podany użytkownik nie istnieje lub nie posiada planów treningowych."));
+
+        return new ArrayList<>(
+            getMapFromPlans(all).values()
+        );
     }
 
     private List<TrainingPlan> getPlansByRoutineId(int trainingRoutineId){
@@ -46,24 +93,69 @@ public class TrainingPlanService {
         );
     }
 
+    private TrainingSchedule prepTrainingSchedule(TrainingPlan plan) {
+        return NoDuplicationService.prepEntity(
+            plan.getTrainingSchedule(),
+            scheduleRepository,
+            scheduleRepository::save
+        );
+    }
+
     public List<TrainingPlan> createNewPlans(List<TrainingPlan> plans){
         // TODO: Finish this method and test it
         List<TrainingPlan> created = new ArrayList<>(plans.size());
         plans.forEach(
             plan ->{
-                plan.setTrainingSchedule(
-                    NoDuplicationService.prepEntity(
-                        plan.getTrainingSchedule(),
-                        scheduleRepository,
-                        scheduleRepository::save
-                    )
-                );
-
+                plan.setTrainingSchedule(prepTrainingSchedule(plan));
                 created.add(repository.save(plan));
             }
         );
 
         return created;
+    }
+
+    public List<TrainingPlan> createNewPlans(TrainingPlansWrite plansWrite, User owner){
+        var weekdays = Weekdays.values();
+        List<TrainingPlan> plans = new ArrayList<>(weekdays.length);
+        TrainingRoutine routine = routineService.createNewByUser(
+            owner
+        );
+
+        for(var weekday : weekdays) {
+            TrainingPlanWrite planWrite = plansWrite.getPlanWriteMap().get(weekday);
+            var trainingId = planWrite.getTrainingId();
+
+            if(trainingId == 0 || !trainingService.existsById(trainingId))
+                continue;
+
+            LocalTime trainingTime = null;
+            if(planWrite.getTrainingTime() != null && !planWrite.getTrainingTime().isEmpty()){
+                String[] time = planWrite.getTrainingTime().split(":");
+                try {
+                    trainingTime = LocalTime.of(
+                        Integer.parseInt(time[0]),
+                        Integer.parseInt(time[1]),
+                        0
+                    );
+                } catch(DateTimeException ignored) { }
+            }
+
+
+            var toAdd = new TrainingPlan(
+                routine,
+                new TrainingSchedule(trainingId, weekday),
+                trainingTime
+            );
+
+            plans.add(toAdd);
+        }
+
+        if(plans.isEmpty()){
+            routineService.delete(routine);
+            throw new IllegalStateException("Nie utworzono planu ponieważ nie przypisano żadnego treningu.");
+        }
+
+        return createNewPlans(plans);
     }
 
 
