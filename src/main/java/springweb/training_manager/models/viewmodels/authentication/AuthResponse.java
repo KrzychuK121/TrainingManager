96d83f2d3b81be3012349f766065cf31e6296d5e
Krzysztof <krzychuk1211@gmail.com>
2024-09-13 10:25:11 +0200

    Creating view model to return token (resolves problem with returning only string. String alone is not JSON serializable).

---
+++ src/main/java/springweb/training_manager/models/viewmodels/authentication/AuthResponse.java
@@ -0,0 +1,12 @@
+package springweb.training_manager.models.viewmodels.authentication;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponse {
+    private String token;
+}
