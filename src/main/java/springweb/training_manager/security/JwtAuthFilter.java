b070c9abfa3a136937fb0566720c8cd1225f073a
Krzysztof <krzychuk1211@gmail.com>
2024-09-13 10:25:53 +0200

    Rearranging filter method and removing old constructor (using annotation for that).

--- src/main/java/springweb/training_manager/security/JwtAuthFilter.java
+++ src/main/java/springweb/training_manager/security/JwtAuthFilter.java
@@ -4,9 +4,9 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -16,17 +16,10 @@ import springweb.training_manager.services.MyUserDetailsService;
 import java.io.IOException;
 
 @Component
+@RequiredArgsConstructor
 public class JwtAuthFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
     private final MyUserDetailsService userDetailsService;
-
-    public JwtAuthFilter(
-        JwtService jwtService,
-        MyUserDetailsService userDetailsService
-    ) {
-        this.jwtService = jwtService;
-        this.userDetailsService = userDetailsService;
-    }
 
     @Override
     protected void doFilterInternal(
@@ -40,38 +33,39 @@ public class JwtAuthFilter extends OncePerRequestFilter {
         }
 
         final String authHeader = request.getHeader("Authorization");
-        final String jwt;
-        final String username;
         var authPrefix = "Bearer ";
 
-        if(authHeader == null || !authHeader.startsWith(authPrefix)){
+        if (authHeader == null || !authHeader.startsWith(authPrefix)) {
             filterChain.doFilter(request, response);
             return;
         }
 
-        jwt = authHeader.substring(authPrefix.length());
-        username = jwtService.extractUsername(jwt);
+        final var jwt = authHeader.substring(authPrefix.length());
+        final String username = jwtService.extractUsername(jwt);
 
-        if(username == null || SecurityContextHolder.getContext().getAuthentication() != null){
+        if (username == null || SecurityContextHolder.getContext().getAuthentication() != null) {
             filterChain.doFilter(request, response);
             return;
         }
 
         MyUserDetails userDetails = (MyUserDetails) userDetailsService.loadUserByUsername(username);
-        if(jwtService.isTokenValid(jwt, userDetails)){
-            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-                userDetails,
-                null,
-                userDetails.getAuthorities()
-            );
-
-            authToken.setDetails(
-                new WebAuthenticationDetailsSource()
-                .buildDetails(request)
-            );
-
-            SecurityContextHolder.getContext().setAuthentication(authToken);
+        if (!jwtService.isTokenValid(jwt, userDetails)) {
+            filterChain.doFilter(request, response);
+            return;
         }
+
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+            userDetails,
+            null,
+            userDetails.getAuthorities()
+        );
+
+        authToken.setDetails(
+            new WebAuthenticationDetailsSource()
+                .buildDetails(request)
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authToken);
 
         filterChain.doFilter(request, response);
     }
