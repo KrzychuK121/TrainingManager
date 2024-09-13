8d80c29e71b1a309d3c8cc7743fa136858a286d6
Krzysztof <krzychuk1211@gmail.com>
2024-09-13 10:29:37 +0200

    Changing login method to return token after login operation.

--- src/main/java/springweb/training_manager/controllers/apis/UserManagerControllerAPI.java
+++ src/main/java/springweb/training_manager/controllers/apis/UserManagerControllerAPI.java
@@ -7,30 +7,31 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
+import springweb.training_manager.models.viewmodels.authentication.AuthResponse;
 import springweb.training_manager.models.viewmodels.user.UserCredentials;
 import springweb.training_manager.services.UserService;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping(
+    value = "/api/auth",
+    produces = MediaType.APPLICATION_JSON_VALUE,
+    consumes = MediaType.APPLICATION_JSON_VALUE
+)
 @RequiredArgsConstructor
 public class UserManagerControllerAPI {
 
     private final UserService userService;
     private static final Logger logger = LoggerFactory.getLogger(UserManagerControllerAPI.class);
 
-    @PostMapping(
-        value = "/login",
-        produces = MediaType.APPLICATION_JSON_VALUE,
-        consumes = MediaType.APPLICATION_JSON_VALUE
-    )
+    @PostMapping(value = "/login")
     @ResponseBody
-    ResponseEntity<?> login(@RequestBody UserCredentials userCredentials){
+    ResponseEntity<?> login(@RequestBody UserCredentials userCredentials) {
         try {
-            var isValid = userService.login(userCredentials);
-            return isValid
-                ? ResponseEntity.noContent().build()
+            var token = userService.login(userCredentials);
+            return token != null
+                ? ResponseEntity.ok(new AuthResponse(token))
                 : ResponseEntity.status(401).body("Wrong login or password.");
-        } catch(UsernameNotFoundException ex) {
+        } catch (UsernameNotFoundException ex) {
             return ResponseEntity.status(404).body("User " + userCredentials.username() + " does not exist.");
         }
     }
