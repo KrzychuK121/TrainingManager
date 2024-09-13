4cb7e3c54fcfe233d24f16b0b418f0b8e72678da
Krzysztof <krzychuk1211@gmail.com>
2024-09-13 10:27:40 +0200

    Adding CORS for API calls. Using annotation for constructor.

--- src/main/java/springweb/training_manager/controllers/MvcConfig.java
+++ src/main/java/springweb/training_manager/controllers/MvcConfig.java
@@ -1,21 +1,23 @@
 package springweb.training_manager.controllers;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class MvcConfig implements WebMvcConfigurer {
     private final WelcomeInfoInterceptor welcomeInfoInterceptor;
-
-    public MvcConfig(
-        final WelcomeInfoInterceptor welcomeInfoInterceptor
-    ) {
-        this.welcomeInfoInterceptor = welcomeInfoInterceptor;
-    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(welcomeInfoInterceptor);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**").allowedOrigins("http://localhost:3000");
     }
 }
