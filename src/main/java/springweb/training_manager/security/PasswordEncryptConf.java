8f74cd09551665352e910d4cd7234225b40a79ad
Krzysztof <krzychuk1211@gmail.com>
2024-09-13 10:28:06 +0200

    Providing documentation that describes why the conf class is necessary.

--- src/main/java/springweb/training_manager/security/PasswordEncryptConf.java
+++ src/main/java/springweb/training_manager/security/PasswordEncryptConf.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+/**
+ * This separate class is required because many beans uses it and if one uses another then the infinity loop
+ * creates.
+ */
 @Configuration
 public class PasswordEncryptConf {
     @Bean
