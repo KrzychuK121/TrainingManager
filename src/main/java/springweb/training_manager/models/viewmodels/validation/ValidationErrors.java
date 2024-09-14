41fccf5294452fc780a3be62289c159ea23c4efe
Krzysztof <krzychuk1211@gmail.com>
2024-09-14 13:10:22 +0200

    Implemented scenario where one field can contain many errors.

--- src/main/java/springweb/training_manager/models/viewmodels/validation/ValidationErrors.java
+++ src/main/java/springweb/training_manager/models/viewmodels/validation/ValidationErrors.java
@@ -5,20 +5,35 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.validation.BindingResult;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 public class ValidationErrors {
-    private Map<String, String> errors;
+    private Map<String, List<String>> errors;
 
     public static ValidationErrors createFrom(BindingResult result) {
-        Map<String, String> errors = new HashMap<>();
+        Map<String, List<String>> errors = new HashMap<>();
 
         result.getFieldErrors().forEach(
-            error -> errors.put(error.getField(), error.getDefaultMessage())
+            error -> {
+                var field = error.getField();
+                var message = error.getDefaultMessage();
+                if (message == null)
+                    message = "Pole nie spełnia wymagań.";
+
+                if (errors.containsKey(field)) {
+                    errors.get(field).add(message);
+                    return;
+                }
+
+                var messages = new ArrayList<>(List.of(message));
+                errors.put(error.getField(), messages);
+            }
         );
 
         return new ValidationErrors(errors);
