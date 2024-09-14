caacc656d92bb6a7a69b851b6bd00f22f576ca14
Krzysztof <krzychuk1211@gmail.com>
2024-09-14 12:14:36 +0200

    Creating view model for not valid registration process.

---
+++ src/main/java/springweb/training_manager/models/viewmodels/authentication/NotValidRegister.java
@@ -0,0 +1,14 @@
+package springweb.training_manager.models.viewmodels.authentication;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class NotValidRegister {
+    private String error;
+}
