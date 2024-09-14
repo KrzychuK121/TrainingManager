74b994a000b783bc31e02df12b89d4a611f5a79c
Krzysztof <krzychuk1211@gmail.com>
2024-09-14 12:16:27 +0200

    Implementing register action. Clearing code a little.

--- src/main/java/springweb/training_manager/controllers/apis/UserManagerControllerAPI.java
+++ src/main/java/springweb/training_manager/controllers/apis/UserManagerControllerAPI.java
@@ -1,5 +1,6 @@
 package springweb.training_manager.controllers.apis;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,10 +8,18 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
+import springweb.training_manager.models.entities.Role;
 import springweb.training_manager.models.viewmodels.authentication.AuthResponse;
+import springweb.training_manager.models.viewmodels.authentication.NotValidRegister;
 import springweb.training_manager.models.viewmodels.user.UserCredentials;
+import springweb.training_manager.models.viewmodels.user.UserWrite;
+import springweb.training_manager.models.viewmodels.validation.ValidationErrors;
 import springweb.training_manager.services.UserService;
+
+import java.util.Set;
 
 @RestController
 @RequestMapping(
@@ -22,9 +31,10 @@ import springweb.training_manager.services.UserService;
 public class UserManagerControllerAPI {
 
     private final UserService userService;
+    private final UserService service;
     private static final Logger logger = LoggerFactory.getLogger(UserManagerControllerAPI.class);
 
-    @PostMapping(value = "/login")
+    @PostMapping("/login")
     @ResponseBody
     ResponseEntity<?> login(@RequestBody UserCredentials userCredentials) {
         try {
@@ -37,7 +47,36 @@ public class UserManagerControllerAPI {
         }
     }
 
-    @PostMapping(value = "/logout")
+    @PostMapping("/register")
+    @ResponseBody
+    ResponseEntity<?> register(
+        @RequestBody @Valid UserWrite current,
+        BindingResult result
+    ) {
+        if (!service.ifPasswordsMatches(current.getPassword(), current.getPasswordRepeat()))
+            result.addError(new FieldError("current", "passwordRepeat", UserService.PASSWORDS_NOT_EQUAL_MESSAGE));
+        
+        if (result.hasErrors()) {
+            var validation = ValidationErrors.createFrom(result);
+            return ResponseEntity.badRequest().body(validation.getErrors());
+        }
+
+        Role roleUser = new Role();
+        roleUser.setName("ROLE_USER");
+
+        try {
+            service.register(current, Set.of(roleUser));
+        } catch (IllegalArgumentException e) {
+            logger.error("Exception occurs: {}", e.getMessage());
+            return ResponseEntity.badRequest().body(
+                new NotValidRegister("User exists")
+            );
+        }
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/logout")
     @ResponseBody
     ResponseEntity<?> logout() {
         SecurityContextHolder.clearContext();
