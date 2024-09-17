fbe00431f6d76e20ac1980a18955b2047e5a7201
Krzysztof <krzychuk1211@gmail.com>
2024-09-17 18:53:24 +0200

    Creating descriptions fields to display on frontend for difficulty and body part enums.

--- src/main/java/springweb/training_manager/models/viewmodels/exercise/ExerciseRead.java
+++ src/main/java/springweb/training_manager/models/viewmodels/exercise/ExerciseRead.java
@@ -1,5 +1,8 @@
 package springweb.training_manager.models.viewmodels.exercise;
 
+import lombok.Getter;
+import springweb.training_manager.models.entities.BodyPart;
+import springweb.training_manager.models.entities.Difficulty;
 import springweb.training_manager.models.entities.Exercise;
 import springweb.training_manager.models.schemas.ExerciseSchema;
 import springweb.training_manager.models.viewmodels.training.TrainingExercise;
@@ -7,8 +10,11 @@ import springweb.training_manager.models.viewmodels.training.TrainingExercise;
 import java.util.ArrayList;
 import java.util.List;
 
+@Getter
 public class ExerciseRead extends ExerciseSchema {
     private List<TrainingExercise> trainings = new ArrayList<>();
+    private final String difficultyDesc;
+    private final String bodyPartDesc;
 
     public ExerciseRead(Exercise exercise) {
         this.id = exercise.getId();
@@ -19,18 +25,16 @@ public class ExerciseRead extends ExerciseSchema {
         this.weights = exercise.getWeights();
         this.time = exercise.getTime();
         this.bodyPart = exercise.getBodyPart();
+        this.bodyPartDesc = BodyPart.getBodyDesc(bodyPart);
         this.difficulty = exercise.getDifficulty();
+        this.difficultyDesc = Difficulty.getEnumDesc(difficulty);
         this.trainings = TrainingExercise.toTrainingExerciseList(exercise.getTrainings());
     }
 
-    public static List<ExerciseRead> toExerciseReadList(final List<Exercise> list){
+    public static List<ExerciseRead> toExerciseReadList(final List<Exercise> list) {
         List<ExerciseRead> result = new ArrayList<>(list.size());
         list.forEach(exercise -> result.add(new ExerciseRead(exercise)));
         return result;
-    }
-
-    public List<TrainingExercise> getTrainings() {
-        return trainings;
     }
 
     public void setTrainings(List<TrainingExercise> trainings) {
