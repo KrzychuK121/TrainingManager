406979f9aa5860ea12217c2dfca017c080f921b0
Krzysztof <krzychuk1211@gmail.com>
2024-09-24 15:18:20 +0200

    Creating method that publish create view model (to use in create form). Reformatting code. Using annotation.

--- src/main/java/springweb/training_manager/services/ExerciseService.java
+++ src/main/java/springweb/training_manager/services/ExerciseService.java
@@ -1,5 +1,6 @@
 package springweb.training_manager.services;
 
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
@@ -8,6 +9,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import springweb.training_manager.models.entities.Exercise;
 import springweb.training_manager.models.entities.Training;
+import springweb.training_manager.models.viewmodels.exercise.ExerciseCreate;
 import springweb.training_manager.models.viewmodels.exercise.ExerciseRead;
 import springweb.training_manager.models.viewmodels.exercise.ExerciseWrite;
 import springweb.training_manager.models.viewmodels.training.TrainingExercise;
@@ -18,18 +20,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class ExerciseService {
     private final ExerciseRepository repository;
     private final TrainingRepository trainingRepository;
+    private final TrainingService trainingService;
     private static final Logger logger = LoggerFactory.getLogger(ExerciseService.class);
-
-    public ExerciseService(
-        final ExerciseRepository repository,
-        final TrainingRepository trainingRepository
-    ) {
-        this.repository = repository;
-        this.trainingRepository = trainingRepository;
-    }
 
     /**
      * This method is used to find existing trainings in database or creating new one
@@ -37,12 +33,11 @@ public class ExerciseService {
      * returned as a new list.
      *
      * @param trainings list of <code>TrainingExercise</code> from <code>ExerciseWrite</code> object.
- *                      Can be used e.g. in <code>create(ExerciseWrite toSave)</code> method.
-     *
+     *                  Can be used e.g. in <code>create(ExerciseWrite toSave)</code> method.
      * @return prepared list with <code>TrainingExercise</code> (founded in database or just created)
      */
-    private List<Training> prepTrainings(List<TrainingExercise> trainings){
-        if(trainings == null || trainings.isEmpty())
+    private List<Training> prepTrainings(List<TrainingExercise> trainings) {
+        if (trainings == null || trainings.isEmpty())
             return null;
 
         List<Training> trainingToSave = new ArrayList<>(trainings.size());
@@ -51,10 +46,10 @@ public class ExerciseService {
                 Training found = trainingRepository.findByTraining(trainingExercise.toTraining())
                     .orElse(trainingExercise.toTraining());
 
-                if(found.getId() == 0){
+                if (found.getId() == 0) {
                     var savedTraining = trainingRepository.save(found);
                     trainingToSave.add(savedTraining);
-                }else
+                } else
                     trainingToSave.add(found);
 
             }
@@ -62,13 +57,13 @@ public class ExerciseService {
         return trainingToSave;
     }
 
-    public Exercise create(ExerciseWrite toSave){
+    public Exercise create(ExerciseWrite toSave) {
         List<Training> preparedTrainingList = prepTrainings(toSave.getTrainings());
-        if(preparedTrainingList != null)
+        if (preparedTrainingList != null)
             toSave.setTrainings(TrainingExercise.toTrainingExerciseList(preparedTrainingList));
 
         var created = repository.save(toSave.toExercise());
-        if(preparedTrainingList != null)
+        if (preparedTrainingList != null)
             editExerciseInTrainings(created, preparedTrainingList, true);
 
         return created;
@@ -81,16 +76,16 @@ public class ExerciseService {
      * many to many row between <code>Exercise</code> and <code>Training</code>
      *
      * @param toAddOrRemove <code>Exercise</code> with id which should be connected with <code>Training</code>
-     * @param toEdit list of <code>Training</code> objects which should be connected with <code>Exercise</code>
-     * @param ifAdd when true, it will add <code>toAddOrRemove</code> to <code>toEdit</code>, otherwise it will
-     *              remove <code>toAddOrRemove</code> from <code>toEdit</code>.
+     * @param toEdit        list of <code>Training</code> objects which should be connected with <code>Exercise</code>
+     * @param ifAdd         when true, it will add <code>toAddOrRemove</code> to <code>toEdit</code>, otherwise it will
+     *                      remove <code>toAddOrRemove</code> from <code>toEdit</code>.
      */
-    private void editExerciseInTrainings(Exercise toAddOrRemove, List<Training> toEdit, boolean ifAdd){
-        if(toEdit == null)
+    private void editExerciseInTrainings(Exercise toAddOrRemove, List<Training> toEdit, boolean ifAdd) {
+        if (toEdit == null)
             return;
         toEdit.forEach(
             training -> {
-                if(ifAdd)
+                if (ifAdd)
                     training.getExercises().add(toAddOrRemove);
                 else
                     training.getExercises().remove(toAddOrRemove);
@@ -99,11 +94,11 @@ public class ExerciseService {
         );
     }
 
-    public Page<ExerciseRead> getAll(Pageable page){
+    public Page<ExerciseRead> getAll(Pageable page) {
         page = PageSortService.validateSort(Exercise.class, page, logger);
 
         Page<ExerciseRead> toReturn = repository.findAll(page).map(ExerciseRead::new);
-        if(toReturn.getContent().isEmpty())
+        if (toReturn.getContent().isEmpty())
             toReturn = repository.findAll(
                 PageRequest.of(
                     PageSortService.getPageNumber(toReturn),
@@ -114,14 +109,25 @@ public class ExerciseService {
         return toReturn;
     }
 
-    public Exercise getById(int id){
+    public Exercise getById(int id) {
         return repository.findById(id)
             .orElseThrow(
                 () -> new IllegalArgumentException("Nie znaleziono ćwiczenia o podanym numerze id")
             );
     }
 
-    public void edit(ExerciseWrite toEdit, int id){
+    public ExerciseCreate getCreateModel(Integer id) {
+        var allTrainings = trainingService.getAll(TrainingExercise::new);
+        if (id == null)
+            return new ExerciseCreate(allTrainings);
+        try {
+            return new ExerciseCreate(new ExerciseRead(getById(id)), allTrainings);
+        } catch (IllegalArgumentException ex) {
+            return new ExerciseCreate(allTrainings);
+        }
+    }
+
+    public void edit(ExerciseWrite toEdit, int id) {
         List<Training> preparedTrainingList = prepTrainings(toEdit.getTrainings());
         toEdit.setTrainings(TrainingExercise.toTrainingExerciseList(preparedTrainingList));
 
@@ -133,7 +139,7 @@ public class ExerciseService {
         editExerciseInTrainings(saved, saved.getTrainings(), true);
     }
 
-    public void delete(int id){
+    public void delete(int id) {
         var toDelete = getById(id);
         editExerciseInTrainings(toDelete, toDelete.getTrainings(), false);
         repository.deleteById(id);
