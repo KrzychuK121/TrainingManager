07813fbd601bd1387767200a76d240d5b44d1cd2
Krzysztof <krzychuk1211@gmail.com>
2024-10-18 14:47:36 +0200

    Creating endpoint for delete operation of training routine.

--- src/main/java/springweb/training_manager/controllers/apis/TrainingRoutineControllerAPI.java
+++ src/main/java/springweb/training_manager/controllers/apis/TrainingRoutineControllerAPI.java
@@ -1,13 +1,16 @@
 package springweb.training_manager.controllers.apis;
 
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+import springweb.training_manager.exceptions.NotOwnedByUserException;
 import springweb.training_manager.models.viewmodels.training_routine.TrainingRoutineRead;
 import springweb.training_manager.services.TrainingRoutineService;
+import springweb.training_manager.services.UserService;
 
 import java.util.List;
 
@@ -19,11 +22,31 @@ import java.util.List;
 )
 @RequiredArgsConstructor
 public class TrainingRoutineControllerAPI {
-
     private final TrainingRoutineService service;
+    private static final Logger logger = LoggerFactory.getLogger(TrainingRoutineControllerAPI.class);
 
     @GetMapping
-    public ResponseEntity<List<TrainingRoutineRead>> getAll(){
+    public ResponseEntity<List<TrainingRoutineRead>> getAll() {
         return ResponseEntity.ok(service.getAll());
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseBody
+    public ResponseEntity<?> delete(
+        @PathVariable int id,
+        Authentication auth
+    ) {
+        var userId = UserService.getUserIdByAuth(auth);
+        try {
+            service.deleteById(id, userId);
+        } catch (NotOwnedByUserException e) {
+            return ResponseEntity.badRequest().build();
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.notFound().build();
+        } catch (Exception ex) {
+            logger.error("Wystąpił nieoczekiwany wyjątek: {}", ex.getMessage(), ex);
+        }
+
+        return ResponseEntity.noContent().build();
     }
 }
