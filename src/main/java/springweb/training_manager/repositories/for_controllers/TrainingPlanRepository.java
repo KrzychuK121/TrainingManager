3be14f8d816e309d6fba54e8d20b1026a7df7ca5
Krzysztof <krzychuk1211@gmail.com>
2024-10-18 14:47:56 +0200

    Removing unused method and reformatting the code.

--- src/main/java/springweb/training_manager/repositories/for_controllers/TrainingPlanRepository.java
+++ src/main/java/springweb/training_manager/repositories/for_controllers/TrainingPlanRepository.java
@@ -9,10 +9,14 @@ import java.util.Optional;
 
 public interface TrainingPlanRepository {
     List<TrainingPlan> findAll();
+
     TrainingPlan save(TrainingPlan entity);
+
     Optional<TrainingPlan> findById(TrainingPlanId trainingPlanId);
+
     Optional<List<TrainingPlan>> findByTrainingRoutineId(int trainingRoutineId);
+
     Optional<List<TrainingPlan>> findByTrainingRoutineOwner(User owner);
+
     boolean existsById(TrainingPlanId trainingPlanId);
-    void deleteById(TrainingPlanId trainingPlanId);
 }
