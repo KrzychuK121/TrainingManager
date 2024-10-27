0f7f4dc7f8ac1857542d4edea4e3e0f0e2f8e86f
Krzysztof <krzychuk1211@gmail.com>
2024-10-27 16:46:57 +0100

    Securing endpoint.

--- src/main/java/springweb/training_manager/controllers/apis/TrainingRoutineControllerAPI.java
+++ src/main/java/springweb/training_manager/controllers/apis/TrainingRoutineControllerAPI.java
@@ -5,9 +5,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import springweb.training_manager.exceptions.NotOwnedByUserException;
+import springweb.training_manager.models.schemas.RoleSchema;
 import springweb.training_manager.models.viewmodels.training_routine.TrainingRoutineRead;
 import springweb.training_manager.services.TrainingRoutineService;
 import springweb.training_manager.services.UserService;
@@ -15,6 +17,7 @@ import springweb.training_manager.services.UserService;
 import java.util.List;
 
 @RestController
+@Secured({RoleSchema.ROLE_ADMIN, RoleSchema.ROLE_USER})
 @RequestMapping(
     value = "/api/routines",
     produces = MediaType.APPLICATION_JSON_VALUE,
