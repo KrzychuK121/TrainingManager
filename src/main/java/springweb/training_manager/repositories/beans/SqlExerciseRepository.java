2f4c77d7dadb8b18c687d5d7c3cf8b6a350493d3
KrzychuK121 <krzychuk1211@gmail.com>
2024-11-14 15:24:05 +0100

    Preparing repo for displaying public or owned by user exercises.

--- src/main/java/springweb/training_manager/repositories/beans/SqlExerciseRepository.java
+++ src/main/java/springweb/training_manager/repositories/beans/SqlExerciseRepository.java
@@ -1,5 +1,6 @@
 package springweb.training_manager.repositories.beans;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Repository;
 import springweb.training_manager.models.entities.Exercise;
 import springweb.training_manager.repositories.for_controllers.ExerciseRepository;
 
+import java.awt.print.Pageable;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,6 +18,23 @@ interface SqlExerciseRepository extends ExerciseRepository, JpaRepository<Exerci
     @Override
     @Query("SELECT e FROM Exercise e LEFT JOIN FETCH e.trainingExercises")
     List<Exercise> findAll();
+
+    @Query("""
+            SELECT e FROM Exercise e
+            WHERE e.owner IS NULL
+                OR e.owner.id = :ownerId
+        """)
+    List<Exercise> findPublicOrOwnedBy(@Param("ownerId") String ownerId);
+
+    @Query("""
+            SELECT e FROM Exercise e
+            WHERE e.owner IS NULL
+                OR e.owner.id = :ownerId
+        """)
+    Page<Exercise> findPublicOrOwnedBy(
+        @Param("ownerId") String ownerId,
+        Pageable pageable
+    );
 
     @Override
     Optional<Exercise> findByIdAndOwnerId(int id, String ownerId);
