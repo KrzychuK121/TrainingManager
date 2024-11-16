76173b1c59fd3d78fd69fe9f79bfe8cf3f8b3de7
KrzychuK121 <krzychuk1211@gmail.com>
2024-11-16 13:57:11 +0100

    Formatting the code and using lombok annotation.

--- src/main/java/springweb/training_manager/services/RoleService.java
+++ src/main/java/springweb/training_manager/services/RoleService.java
@@ -1,31 +1,27 @@
 package springweb.training_manager.services;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import springweb.training_manager.models.entities.Role;
 import springweb.training_manager.repositories.for_controllers.RoleRepository;
 
 import java.util.Optional;
 
+@RequiredArgsConstructor
 @Service
 public class RoleService {
     private final RoleRepository repository;
 
-    public RoleService(
-        final RoleRepository repository
-    ) {
-        this.repository = repository;
-    }
-
-    public Role save(Role toSave){
+    public Role save(Role toSave) {
         return repository.save(toSave);
     }
 
-    public Role getRoleByName(String name){
+    public Role getRoleByName(String name) {
         return repository.findByName(name)
-        .orElseThrow(() -> new IllegalArgumentException("Rola o podanej nazwie nie istnieje."));
+            .orElseThrow(() -> new IllegalArgumentException("Rola o podanej nazwie nie istnieje."));
     }
 
-    public Optional<Role> getOptionalRoleByName(String name){
+    public Optional<Role> getOptionalRoleByName(String name) {
         return repository.findByName(name);
     }
 }
