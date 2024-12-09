8187412dc2bf4762f5f45831b0bcef5abbafa7e2
KrzychuK121 <krzychuk1211@gmail.com>
2024-12-09 11:43:36 +0100

    Creating new fields that will contain preferred start training time for users new routine.

--- src/main/java/springweb/training_manager/models/viewmodels/workout_assistant/WorkoutAssistantWrite.java
+++ src/main/java/springweb/training_manager/models/viewmodels/workout_assistant/WorkoutAssistantWrite.java
@@ -1,9 +1,12 @@
 package springweb.training_manager.models.viewmodels.workout_assistant;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import org.hibernate.validator.constraints.Range;
+
+import java.time.LocalTime;
 
 @Getter
 @Setter
@@ -13,4 +16,8 @@ public class WorkoutAssistantWrite {
     private WeightReductionWrite weightReduction;
     @Range(min = 1, max = 7, message = "Liczba dni treningowych musi być w przedziale 1-7")
     private int workoutDays;
+    @NotNull
+    private LocalTime earliestTrainingStart;
+    @NotNull
+    private LocalTime latestTrainingStart;
 }
