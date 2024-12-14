d354b531990b1825a641fa0ffb7d06342031b6c1
KrzychuK121 <krzychuk1211@gmail.com>
2024-12-14 18:01:17 +0100

    Providing training time info to display for user.

--- src/main/java/springweb/training_manager/models/viewmodels/workout_assistant/PlannedTrainingRead.java
+++ src/main/java/springweb/training_manager/models/viewmodels/workout_assistant/PlannedTrainingRead.java
@@ -5,14 +5,21 @@ import springweb.training_manager.models.entities.Training;
 import springweb.training_manager.models.viewmodels.training.TrainingRead;
 import springweb.training_manager.services.TrainingService;
 
+import java.time.LocalTime;
+
 @Getter
 public class PlannedTrainingRead {
     private final TrainingRead training;
     private final int totalBurnedKcal;
+    private final LocalTime trainingTime;
 
-    public PlannedTrainingRead(Training training) {
+    public PlannedTrainingRead(
+        Training training,
+        LocalTime trainingTime
+    ) {
         var trainingRead = new TrainingRead(training);
         this.training = trainingRead;
         totalBurnedKcal = TrainingService.getTotalBurnedKcal(trainingRead);
+        this.trainingTime = trainingTime;
     }
 }
