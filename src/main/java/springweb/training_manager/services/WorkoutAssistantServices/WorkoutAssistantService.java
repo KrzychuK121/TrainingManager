7aa439a6aa4b21b683652562795d40d9a8cd7f36
KrzychuK121 <krzychuk1211@gmail.com>
2024-12-18 14:40:12 +0100

    Finishing first impl of saving created routine

--- src/main/java/springweb/training_manager/services/WorkoutAssistantServices/WorkoutAssistantService.java
+++ src/main/java/springweb/training_manager/services/WorkoutAssistantServices/WorkoutAssistantService.java
@@ -1,5 +1,6 @@
 package springweb.training_manager.services.WorkoutAssistantServices;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -8,14 +9,10 @@ import springweb.training_manager.models.entities.*;
 import springweb.training_manager.models.viewmodels.exercise_parameters.ExerciseParametersRead;
 import springweb.training_manager.models.viewmodels.training.TrainingRead;
 import springweb.training_manager.models.viewmodels.validation.ValidationErrors;
-import springweb.training_manager.models.viewmodels.workout_assistant.MuscleGrowWrite;
-import springweb.training_manager.models.viewmodels.workout_assistant.PlannedRoutineRead;
-import springweb.training_manager.models.viewmodels.workout_assistant.WeightReductionWrite;
-import springweb.training_manager.models.viewmodels.workout_assistant.WorkoutAssistantWrite;
-import springweb.training_manager.repositories.for_controllers.ExerciseParametersRepository;
-import springweb.training_manager.repositories.for_controllers.TrainingRepository;
-import springweb.training_manager.repositories.for_controllers.TrainingScheduleRepository;
+import springweb.training_manager.models.viewmodels.workout_assistant.*;
+import springweb.training_manager.repositories.for_controllers.*;
 import springweb.training_manager.services.ExerciseParametersService;
+import springweb.training_manager.services.NoDuplicationService;
 import springweb.training_manager.services.TrainingService;
 
 import java.time.LocalTime;
@@ -27,19 +24,113 @@ import java.util.concurrent.*;
 @Slf4j
 public class WorkoutAssistantService {
     private final TrainingRepository trainingRepository;
+    private final TrainingExerciseRepository trainingExerciseRepository;
+    private final TrainingRoutineRepository trainingRoutineRepository;
     private final TrainingScheduleRepository trainingScheduleRepository;
+    private final TrainingPlanRepository trainingPlanRepository;
     private final ExerciseParametersRepository parametersRepository;
 
     private static final Map<User, TrainingRoutine> plannedUsersRoutines = Collections.synchronizedMap(new HashMap<>());
 
+    private Training prepareTrainingToSave(
+        Training training,
+        TrainingRoutine routine
+    ) {
+        if (
+            !training.getId()
+                .equals(training.defaultId())
+        )
+            return training;
+
+        var otherTraining = new Training();
+        otherTraining.copy(training);
+        otherTraining.setTrainingExercises(List.of());
+        otherTraining.setTitle(
+            String.format(
+                "%s#%d",
+                otherTraining.getTitle(),
+                routine.getId()
+            )
+        );
+
+        var found = trainingRepository.findDuplication(otherTraining);
+        return found.orElseGet(
+            () -> trainingRepository.save(otherTraining)
+        );
+    }
+
+    @Transactional
     public void savePlannedRoutine(User user) {
-        var retrieved = plannedUsersRoutines.get(user);
-        if (retrieved == null)
+        var routine = plannedUsersRoutines.get(user);
+        if (routine == null)
             throw new IllegalStateException(
                 "User " + user.getUsername() + " does not have planned routine yet."
             );
 
-        // TODO: Implement logic to save whole routine data if missing in database
+        var newRoutine = new TrainingRoutine();
+        newRoutine.setOwner(user);
+
+        var savedRoutine = trainingRoutineRepository.save(newRoutine);
+        var plans = routine.getPlans();
+        plans.forEach(
+            plan -> {
+                var schedule = plan.getTrainingSchedule();
+                var training = schedule.getTraining();
+                var preparedTraining = prepareTrainingToSave(
+                    training,
+                    newRoutine
+                );
+
+                schedule.setTraining(preparedTraining);
+                if (
+                    preparedTraining.getTrainingExercises()
+                        .isEmpty()
+                ) {
+                    List<TrainingExercise> preparedTrainingExercises = training.getTrainingExercises()
+                        .stream()
+                        .map(
+                            trainingExercise -> {
+                                var preparedParameters = NoDuplicationService.prepEntity(
+                                    trainingExercise.getParameters(),
+                                    parametersRepository,
+                                    parametersRepository::save
+                                );
+                                trainingExercise.setParameters(preparedParameters);
+                                trainingExercise.setTraining(preparedTraining);
+
+                                return trainingExerciseRepository.save(
+                                    new TrainingExercise(
+                                        preparedTraining,
+                                        trainingExercise.getExercise(),
+                                        preparedParameters
+                                    )
+                                );
+                            }
+                        )
+                        .toList();
+
+                    preparedTraining.setTrainingExercises(preparedTrainingExercises);
+                }
+
+                var preparedSchedule = NoDuplicationService.prepEntity(
+                    new TrainingSchedule(
+                        preparedTraining,
+                        schedule.getWeekday()
+                    ),
+                    trainingScheduleRepository,
+                    trainingScheduleRepository::save
+                );
+
+                var newTrainingPlan = new TrainingPlan(
+                    savedRoutine,
+                    preparedSchedule,
+                    plan.getTrainingTime()
+                );
+                trainingPlanRepository.save(newTrainingPlan);
+            }
+        );
+        
+        plannedUsersRoutines.remove(user);
     }
 
     public ValidationErrors validateAndPrepare(
