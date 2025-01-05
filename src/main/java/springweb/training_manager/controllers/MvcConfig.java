d250f7fdbbc2507c6345cdb5bdb5294b441fe3db
KrzychuK121 <krzychuk1211@gmail.com>
2025-01-05 20:37:37 +0100

    Moving configuration to .env file.

--- src/main/java/springweb/training_manager/controllers/MvcConfig.java
+++ src/main/java/springweb/training_manager/controllers/MvcConfig.java
@@ -1,6 +1,7 @@
 package springweb.training_manager.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -9,6 +10,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @RequiredArgsConstructor
 public class MvcConfig implements WebMvcConfigurer {
+    @Value("${security.frontend.url}")
+    private String frontendUrl;
+    
     private final WelcomeInfoInterceptor welcomeInfoInterceptor;
     private final String[] ALLOWED_METHODS = {
         "GET", "POST", "PUT", "PATCH", "DELETE"
@@ -22,7 +26,7 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-            .allowedOrigins("http://localhost:3000")
+            .allowedOrigins(frontendUrl)
             .allowedMethods(ALLOWED_METHODS);
     }
 }
